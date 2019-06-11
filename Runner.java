209a60c57c9a2cf7c1387b2b9cb120eca2856107
Yash Vidyarthi <10018568@sbstudents.org>
2019-06-11 07:48:44 -0400

    Combined all work into a single file

--- Runner.java
+++ Runner.java
@@ -1,5 +1,18 @@
 public class Runner{
     public static void main(String [] args){
-    
+    	PrintQuotientInterface variable = (float a, float b) -> PrintQuotient(a,b);
+		variable.PrintQuotient((float)1.2,(float)1.3);
+
+		PerfectSquare ps = (int perfectSquare) -> {
+			for(int x = 0;x<perfectSquare;x++){
+				if(x*x == perfectSquare)
+					return true;
+		}
+		return false;};
+	System.out.println(ps.perfectSquare(25));
+
     }
+   	public static void PrintQuotient(float a, float b){
+   		System.out.printf("%.3f", a/b );
+	}
 }
